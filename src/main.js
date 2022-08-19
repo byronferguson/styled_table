9ead11ef80ea5584fe291270ec070750369e22f4
byronferguson <ferguson.bs@gmail.com>
2022-08-18 19:12:02 -0500

    1 file modified, 1 added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
+import tooltip from './directives/tooltip';
 import App from './App.vue';
 import './index.css';
+
+app.directive('tooltip', tooltip);
 
 createApp(App).mount('#app');
